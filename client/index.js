69ecc2edcc0b1cc498c86e4daca7387573df0407
denskarlet <den.skarlet@gmail.com>
2020-07-15 14:04:03 -0400

    context comment

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import LocalContext from './context.jsx';
-
+// state we want to shate with entire component tree
 const localization = {
   english: {
     question: 'what the hell?',
@@ -16,14 +16,19 @@ const localization = {
     suggestion: 'уйди отсюда',
   },
 };
+// just a wrapper component to provide App with context
 const Wrapper = () => {
+  // default language is now 'english', setLanguage is a function to change the language
   const [language, setLanguage] = useState('english');
   return (
+    // wrap App with context provider, value={} is what we can now access from anywhere
+    // what language, piece of localization under the key of the language, and a function to change context.
     <LocalContext.Provider value={[language, localization[language], setLanguage]}>
       <App />
     </LocalContext.Provider>
   );
 };
+// render wrapper component that will render app ^
 ReactDOM.render(
   <Router>
     <Wrapper />
