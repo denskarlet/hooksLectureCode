077bf8cfbae0cf281b0c6ff6649cc9fecefb58bf
denskarlet <den.skarlet@gmail.com>
2020-07-15 14:06:15 -0400

    minor

--- client/components/UseContext2.jsx
+++ client/components/UseContext2.jsx
@@ -4,7 +4,9 @@ import LocalContext from '../context.jsx';
 const UseContext2 = () => {
   const [language, data, setLanguage] = useContext(LocalContext);
   const prettyJSON = JSON.stringify(data, null, 2);
-
+  // this component just renders provided data from the context
+  // its here to shows that when you change the context in UseContext.jsx - it'll be reflected here as well
+  // context is gloooooabal and it will cause rerender of every component that is 'subscribed' to it
   return (
     <div>
       <pre>{prettyJSON}</pre>
